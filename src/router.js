1ae53db90309a392860387e6363e6473e38401e1
Bruce <bruce_ly@qq.com>
2018-12-04 15:18:45 +0800

    commit

--- src/router.js
+++ src/router.js
@@ -4,25 +4,25 @@ import Router from 'vue-router'
 Vue.use(Router)
 
 export default new Router({
-    routes: [
-        {
-            path: '/',
-            name: 'home',
-            // route level code-splitting
-            // this generates a separate chunk (about.[hash].js) for this route
-            // which is lazy-loaded when the route is visited.
-            component: () => import(/* webpackChunkName: "about" */ './views/home.vue'),
-        },
-        {
-            path: '/about',
-            name: 'about',
-            component: () => import('./views/about.vue'),
-        },
-        {
-            path: '/demo',
-            name: 'demo',
-            component: () => import('./views/demo.vue'),
-        },
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import(/* webpackChunkName: "about" */ './views/home.vue')
+    },
+    {
+      path: '/about',
+      name: 'about',
+      component: () => import('./views/about.vue')
+    },
+    {
+      path: '/demo',
+      name: 'demo',
+      component: () => import('./views/demo.vue')
+    }
 
-    ]
+  ]
 })
